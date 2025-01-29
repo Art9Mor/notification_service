4a612414c484a79b27babde10c28f20eb01b36b2
Art9Mor <deusestmaleficus@gmail.com>
2025-01-29 13:28:45 +0700

    core was creating

--- app/core/celery.py
+++
@@ -1,9 +0,0 @@
-from celery import Celery
-
-from .config import settings
-
-celery_app = Celery(
-    'notification_tasks',
-    broker=settings.CELERY_BROKER_URL,
-    backend=settings.CELERY_BACKEND_URL
-)
